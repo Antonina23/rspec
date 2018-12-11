75ccf1bde71b2ada225aca08ae1d7a24cbe28529
Antonina23 <tone4kawow@gmail.com>
2018-12-11 20:35:58 +0300

    Refactoring tests

--- herospec.rb
+++ herospec.rb
@@ -24,3 +24,29 @@ describe Hero do
     expect(hero.hero_info).to eq 'Mike has a health of 100'
   end
 end
+
+# можно сократить и вынести hero new отдельно
+require './hero'
+
+describe Hero do
+
+  before do
+  @hero = Hero.new 'mike'
+  end
+
+  it "has a capitalized name" do
+    expect(@hero.name).to eq 'Mike' #аналог в ruby: hero.name == 'Mike'
+  end
+
+  it "can power up" do
+    expect(@hero.power_up).to eq 110
+  end
+
+  it "can power down" do
+    expect(@hero.power_down).to eq 90
+  end
+
+  it "displays full hero info" do
+    expect(@hero.hero_info).to eq 'Mike has a health of 100'
+  end
+end
